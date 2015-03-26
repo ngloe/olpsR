8e3d34464532863d9abe5aec2150a8ee9b33442d
ngloe <ngl@gmx.net>
2015-03-26 13:10:11 +0100

    update NYSE help

--- R/data_NYSE.R
+++ R/data_NYSE.R
@@ -4,12 +4,11 @@
 
 #' NYSE daily returns
 #' 
-#' The dataset contains daily returns of the New York Stock Exchange 
-#' of 36 stocks from 1962-07-03 until 1984-12-31, that is 5651 trading days. 
+#' The dataset contains daily returns of 36 stocks listed in the 
+#' New York Stock Exchange from 1962-07-03 until 1984-12-31, that is 5651 trading days. 
 #' Returns are calculated as closing price divided by the closing price 
 #' of the privious day (price relative). 
-#' The dataset was used, amongst others, by Cover for his analysis of 
-#' 'Universal Portfolios'. 
+#' The dataset was used for the analysis of Cover's \code{Universal Portfolio} algorithm for example
 #' 
 #' @format A data frame with 5651 observations on the following 36 stocks.
 #' 
@@ -55,8 +54,12 @@
 #' 
 #' @usage data(NYSE)
 #' 
-#' @source According to Helmbold et al, the data set was originally collected by Hal Stern. 
-#' The data here is provided by Yoram Singer \url{http://www.cs.bme.hu/~oti/portfolio/data.html}
+#' @source Originally collected by Hal Stern the data here is provided by 
+#' Yoram Singer \url{http://www.cs.bme.hu/~oti/portfolio/data.html}
+#' 
+#' @references
+#' Cover, T. M. 
+#' Universal Portfolios, 1991
 #' 
 #' @docType data
 #' @keywords datasets
