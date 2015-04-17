b20b0ca1d7db79f1a77d47b7602dd58abaa8a4c4
ngloe <ngl@gmx.net>
2015-04-17 11:33:25 +0200

    - add simplex projection function by Duchi et al. and set it as primary function for simplex projection -> projsplx - rename existing func to projsplx_2)

--- R/projsplx.R
+++ R/projsplx.R
@@ -1,46 +1,48 @@
 # ====================================== #
 # Projection onto a simplex
-# Yunmei Chen, Xiaojing Ye
+# Implemented according to the paper: Efficient projections onto the
+# l1-ball for learning in high dimensions, John Duchi, et al. ICML 2008.
 # Paper link: 
-#  http://ufdc.ufl.edu/IR00000353/
-#  http://arxiv.org/abs/1101.6081
+#  https://web.stanford.edu/~jduchi/projects/DuchiShSiCh08.pdf
+#  https://dl.acm.org/citation.cfm?id=1390191
 #
 #### roxygen2 comments ################################################
 #
-#' Projection onto a simplex
+#' Projection onto simplex
 #' 
-#' computes the projection of a vector onto the simplex 
-#' \eqn{\Delta^n}
+#' computes the Euclidean projection of points onto the simplex domain
+#' \eqn{\Delta^n} of specified radius
 #' 
-#' @param y vector to project onto the simplex
+#' @param v vector of points to project onto the simplex domain
+#' @param b radius
 #' 
-#' @return Vector
+#' @return Projection vector
 #'  
-#' @details The algorithm is based on a mini-paper by Chen and Ye (see refereneces)
+#' @details The algorithm is implemented according to Duchi et al. 2008.
+#' The original Matlab implementation by John Duchi (\email{jduchi@@cs.berkeley.edu})
+#' can be found on \url{https://web.stanford.edu/~jduchi/projects/DuchiShSiCh08/ProjectOntoSimplex.m}
 #'
 #' @references 
-#' Yunmei Chen, Xiaojing Ye 2011. Projection Onto A Simplex
-#' \url{http://arxiv.org/abs/1101.6081v2}
+#' Duchi, J.; Shalev-Shwartz, S.; Singer, Y. & Chandra, T. 
+#' Efficient projections onto the l 1-ball for learning in high dimensions, 
+#' Proceedings of the 25th international conference on Machine learning, 2008, 272-279
+#' \url{https://dl.acm.org/citation.cfm?id=1390191}
 #'       
 #' @examples 
-#' y = runif(100, -10, 10)
-#' projsplx(y)
+#' x = runif(100, -10, 10)
+#' projsplx(x)
 #' 
 #' @export
 #' 
 #########################################################################
-projsplx = function(y){
-  n = length(y)
-  s = sort(y, decreasing = TRUE)
-  tmpsum = cumsum(s[1:(n-1)])
-  tmp = ( tmpsum - 1 ) / (1:(n-1))
-  ind = which( tmp >= s[2:n] )[1]
-  if( !is.na(ind) ){
-    t = tmp[ind]
-  } else {
-    t = ( tmpsum[n-1] + s[n] - 1 ) / n
-  }
-  x = apply(cbind( y-t, 0 ), 1, max)
-  return(x)
+projsplx = function(v, b=1){
+  v[v<0] = 0
+  u = sort(v, decreasing = TRUE)
+  sv = cumsum(u)
+  
+  rho = which(u > (sv-b) / (1:length(v)))
+  theta = max(0, (sv[rho] - b) / rho )
+  w = v - theta
+  w[w<0] = 0
+  return(w)
 }
-
